9c31d666cf4886c649f4b930ffe426a7eb68d4a4
taysueb <tay.chaicharn@gmail.com>
2017-10-31 13:49:00 +0700

    Initial Commit

---
+++ Anti-biotic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Anti-biotic
+//
+//  Created by Chaicharn Vijarnsatit on 10/31/2560 BE.
+//  Copyright © 2560 Chaicharn Vijarnsatit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
